109963da8b26027e705c91409b3ae8eedb6d4249
Artimirche6 <artimirche6@gmail.com>
2021-10-02 02:02:32 +0530

    [arti] Dice Roll and Position Updated

--- snakeLadder.java
+++ snakeLadder.java
@@ -5,5 +5,11 @@ public class snakeLadder
 		int position = 0;
 		System.out.println(" Initial Position : " +position);
 		
+		int number = (int)(Math.random()*6)+1;
+		System.out.println("Dice Number : " +number);
+		
+		number += position;
+		System.out.println("Current Position : " +number);
+		
 	}
 }
