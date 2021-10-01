e4b74dc7ce062f31f67b53b45a3e3f93e7a5e7d8
Artimirche6 <artimirche6@gmail.com>
2021-10-02 01:57:01 +0530

    [arti] Start Position

--- snakeLadder.java
+++ snakeLadder.java
@@ -2,6 +2,8 @@ public class snakeLadder
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Snake and Ladder Game");
+		int position = 0;
+		System.out.println(" Initial Position : " +position);
+		
 	}
 }
