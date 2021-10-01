2814a40234c76ce81e4991263784157271957d66
Artimirche6 <artimirche6@gmail.com>
2021-10-02 03:13:46 +0530

    [arti] Check for Option

--- snakeLadder.java
+++ snakeLadder.java
@@ -1,15 +1,55 @@
 public class snakeLadder
 {
+	
 	public static void main(String[] args)
 	{
-		int position = 0;
+		int position = 10;
 		System.out.println(" Initial Position : " +position);
 		
-		int number = (int)(Math.random()*6)+1;
-		System.out.println("Dice Number : " +number);
+		int option  = (int)(Math.random()*3)+1;
+		System.out.println("Option : " +option);
 		
-		number += position;
-		System.out.println("Current Position : " +number);
+		switch(option)
+		{
+			case 1: 
+				int diceRoll  = (int)(Math.random()*6)+1;
+				System.out.println("Dice Number : " +diceRoll);
+				
+				position += diceRoll;
+				System.out.println("Current Position : " +position);
+				
+			break;	
+				
+			case 2: 
+				
+				if(position == 0)
+				{
+					position = 0;
+					System.out.println("Start Position : " +position);
+				}
+				else
+				{
+					int diceRoll1  = (int)(Math.random()*6)+1;
+					System.out.println("Dice Number : " +diceRoll1);
+				
+					position -= diceRoll1;
+					System.out.println("Current Position : " +position);
+				}
+				
+			break;	
+			
+			case 3: 
+				System.out.println(" No Play : " +position);
+			break;
+			
+			default:
+				System.out.println("Player Quit");
+			break;
+			
+				
+			
+		}
+		
 		
 	}
 }
