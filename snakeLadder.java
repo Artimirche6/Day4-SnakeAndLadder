96e9c640d918a14ea7efc2004e76855918039873
Artimirche6 <artimirche6@gmail.com>
2021-10-16 19:40:03 +0530

    [arti] 2 Player Game

--- snakeLadder.java
+++ snakeLadder.java
@@ -92,5 +92,95 @@ public class snakeLadder
 		}
 		
 		
+		//2nd Player
+		
+		int diceRoll1 = 0, position1 = 0, diceCount1 = 0, optionCount1 = 0;
+		System.out.println(" *******Initial Position for Player 2 : " +position1+"*******");
+
+		while(position1 <= 100)
+		{
+		
+		int option1  = (int)(Math.random()*3)+1;
+		System.out.println("Option for Player 2 : " +option1);
+		
+		optionCount1++;
+		System.out.println("#------Option Count for Player 2 : " +optionCount1+ "------#");
+		
+			switch(option1)
+			{
+				case 1: 
+					
+					diceRoll1  = (int)(Math.random()*6)+1;
+					System.out.println("Dice Number for Player 2 : " +diceRoll1);
+						
+					position1 += diceRoll1;
+					diceCount1++;
+					System.out.println("----Dice Count for Player 2 : " +diceCount1+ "----");
+					
+					if (position1 > 100)
+					{
+						position1 -= diceRoll1;
+						System.out.println("No Move Position (More than 10) for Player 2 : " +position1);
+					}
+					else if(position1 == 100)
+					{
+						System.out.println("Player 2 WON : " +position1);
+						System.exit(0);
+					}
+					else
+					{
+						System.out.println("Current Position  for Player 2 : " +position1);
+					}
+					
+				break;	
+					
+				case 2: 
+					
+					if(position1 == 0)
+					{
+						position1 = 0;
+						System.out.println("Initial Start Position  for Player 2 : " +position1);
+					}
+					else if(position1 == 100)
+					{
+						System.out.println("Player 2 WON : " +position1);
+						System.exit(0);
+					}
+					else
+					{
+						diceRoll1  = (int)(Math.random()*6)+1;
+						System.out.println("Dice Number for Player 2 : " +diceRoll1);
+						
+						diceCount1++;
+						System.out.println("----Dice Count for Player 2 : " +diceCount1+ "----");
+					
+						position1 -= diceRoll1;
+						if(position1 < 0)
+						{
+							position1 += diceRoll1;
+							System.out.println(" No Move Position (Less than 0) for Player 2 : " +position1);
+						}
+						else
+						{
+							System.out.println("Current Position for Player 2: " +position1);
+						}
+					}
+					
+				break;	
+				
+				case 3: 
+					System.out.println(" No Play for Player 2: " +position1);
+				break;
+				
+				default:
+					System.out.println("Player 2 Quit");
+				break;
+				
+					
+				
+			}
+			
+		}
+		
 	}
 }
